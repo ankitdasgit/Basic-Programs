139fe4d098371663d944891392c1ade252921705
ankit das <ankit.das2019@sait.ac.in>
2022-07-29 22:59:42 +0530

    Possitive Negative Number Check

---
+++ src/basicPrograms/PossitiveNagative.java
@@ -0,0 +1,18 @@
+package basicPrograms;
+
+public class PossitiveNagative {
+    
+	public static void main(String[] args) {
+		
+	int n=56;
+	
+	if(n>0) {
+		System.out.println("possitive");
+	}else if(n<0) {
+		System.out.println("negative");
+	}else
+		System.out.println("zero");
+		
+	}
+
+}
