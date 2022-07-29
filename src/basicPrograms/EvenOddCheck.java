709f836d8f8a5a4749eb1e8a4d059c5b8b77893c
ankit das <ankit.das2019@sait.ac.in>
2022-07-29 23:30:30 +0530

    Even Odd check program

---
+++ src/basicPrograms/EvenOddCheck.java
@@ -0,0 +1,17 @@
+package basicPrograms;
+
+public class EvenOddCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=0;
+		
+	 if(n%2==0) {
+		 System.out.println("even");
+	 }else
+		 System.out.println("odd");
+
+	}
+
+}
